749e3e159c54f9f1ad72a37af0555e8cc0980210
dhruv10032808 <dhruv.bhanu.1003@gmail.com>
2023-02-12 20:21:57 +0530

    fetch data

--- src/App.js
+++ src/App.js
@@ -4,63 +4,31 @@ import { useSelector, useDispatch } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { uiActions } from './store/ui-slice';
+import { sendCartData,fetchCartData } from './store/cart-action';
 import Notification from './components/UI/Notification';
 
 let isInitial = true;
 
 function App() {
   const dispatch = useDispatch();
+  const changed=useSelector((state)=>state.cart.changed)
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'pending',
-          title: 'Sending...',
-          message: 'Sending cart data!',
-        })
-      );
-      const response = await fetch(
-        'https://redux-cart-1cee6-default-rtdb.firebaseio.com/cart.json',
-        {
-          method: 'PUT',
-          body: JSON.stringify(cart),
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error('Sending cart data failed.');
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: 'success',
-          title: 'Success!',
-          message: 'Sent cart data successfully!',
-        })
-      );
-    };
-
     if (isInitial) {
       isInitial = false;
       return;
     }
-
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'error',
-          title: 'Error!',
-          message: 'Sending cart data failed!',
-        })
-      );
-    });
+    if(changed){
+    dispatch(sendCartData(cart));
+    }
   }, [cart, dispatch]);
 
+  useEffect(()=>{
+      dispatch(fetchCartData())
+  },[dispatch])
   return (
     <Fragment>
       {notification && (
